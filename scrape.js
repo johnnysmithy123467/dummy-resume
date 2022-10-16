000aca8de377af17b4856a8eeb60c077305c0d34
chubbyFreak <johnny.smithy.123467@gmail.com>
2022-10-16 04:28:44 -0400

    updated scrape.js for addition of resume.tex

--- scrape.js
+++ scrape.js
@@ -8,19 +8,25 @@
 // @description 10/16/2022, 2:11:51 AM
 // ==/UserScript==
 
+const API_URL = 'https://maheshnat.herokuapp.com/api/update-resume';
+const SECRET =
+  '+CiMojHIwIGk5jD6yPPAip0uAB1b8TH+18suSWCjWXD0XoyN2BNAFne2ezpPk23e/LOAPPBe2IYn2wpQEDwXQuT9gNQARq7NCq/fNjj/yCWr8SNPl/og4yj24q1Nop01V6qJoD8Io5MVDXXIE6kcyslj9rVKfHvtLbgkoz29rxNhfVoXyeKeqWPzv2Y6U36F4YNsR2rQZH/BxOJ1NInQcu+etzD6G/GuSh48Cw2uf4X6skT6pZE59GBKcA3Hew6g9nJJpk4PZAyrDWNRftBKU+ickJ8gd+YfU6N1pQqCV7sE9IQ5nMvOOaWepnjlLRsBfr0DrVxC3YS+8XkvlxEKIQ==';
+
 const pushToGithub = async () => {
   let res = await fetch(
     document.querySelector('.fa-download').parentElement.href
   );
 
   const encodedText = btoa(unescape(encodeURIComponent(await res.text())));
-  const latex = await navigator.clipboard.readText();
+  const latex = btoa(
+    unescape(encodeURIComponent(await navigator.clipboard.readText()))
+  );
 
   try {
-    res = await fetch('https://maheshnat.herokuapp.com/api/update-resume', {
+    res = await fetch(API_URL, {
       method: 'PUT',
       headers: {
-        secret: 'secret',
+        secret: SECRET,
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
